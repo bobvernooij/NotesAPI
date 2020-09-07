aadefd23a9d41785f017aace4008f9929f1b34fb
Cornelis de Mooij <codemo@gmail.com>
2020-09-07 16:23:24 +0200

    Reduced to 1 endpoint

--- src/main/java/com/cornelisdemooij/NotesAPI/services/NoteService.java
+++ src/main/java/com/cornelisdemooij/NotesAPI/services/NoteService.java
@@ -6,44 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.Optional;
 
 @Service
 @Transactional
 public class NoteService {
+
     @Autowired
     private NoteRepository noteRepository;
-
-    public Note save(Note note) {
-        note.creation = Timestamp.from(Instant.now());
-        note.modified = Timestamp.from(Instant.now());
-        return noteRepository.save(note);
-    }
-
-    public Optional<Note> findById(Long id) {
-        return noteRepository.findById(id);
-    }
 
     public Iterable<Note> findAll() {
         return noteRepository.findAll();
     }
 
-    public Optional<Note> updateById(Long id, Note newNote) throws Exception {
-        Optional<Note> optionalOldNote = findById(id);
-        if (optionalOldNote.isPresent()) {
-            Note oldNote = optionalOldNote.get();
-            newNote.id = oldNote.id;
-            newNote.creation = oldNote.creation;
-            newNote.modified = Timestamp.from(Instant.now());
-            return Optional.of(noteRepository.save(newNote));
-        } else {
-            throw new Exception("Error: tried to update a note that does not exist.");
-        }
-    }
-
-    public void deleteById(Long id) {
-        noteRepository.deleteById(id);
-    }
 }
